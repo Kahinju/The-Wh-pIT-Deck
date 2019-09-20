0378e172cdc260bac5c894437cfbf9904fcbae6f
Kahinju <54955246+Kahinju@users.noreply.github.com>
2019-09-20 02:24:31 -0400

    Add files via upload
    BoxCollider and Rigidbody needs to be added to each game object. IsTrigger should be checked under the BoxCollider component.

---
+++ GameController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+
+public class GameController : MonoBehaviour
+{
+    public GameObject player;
+
+    public GameObject enemy;
+
+    private BoxCollider2D enemy_collider;
+
+    private BoxCollider2D player_collider;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy_collider = enemy.GetComponent<BoxCollider2D>() as BoxCollider2D;
+
+        player_collider = player.GetComponent<BoxCollider2D>() as BoxCollider2D;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+        detectPlayer();
+    }
+
+    void detectPlayer()
+    {
+        bool test = enemy_collider.IsTouching(player_collider);
+
+        if (test)
+        {
+            var rend = enemy.GetComponent<Image>();
+            rend.material.SetColor("_Color", Color.red);
+        }
+        
+    }
+
+    void movePlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            player.transform.position += Vector3.left * 5f;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            player.transform.position += Vector3.right * 5f;
+        }
+    }
+    
+}
+
+ 
+
+
